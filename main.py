6edae5b82eca23fb0c4a011c5aeb73ef69f038a0
Hunter Boyd <hunterjsb@gmail.com>
2021-10-08 23:30:45 -0400

    commit to add PartyRockFire.jpeg - and some test code - not a solution

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 # main
+
+import numpy as np
+import cv2 as cv
+import time
+# OPENCV IS --- BLUE, GREEN, RED --- (BGR)
+"""
+the image is 860 x 679 pixels (679 rows x 860 columns)
+need to loop through about ~600k pixels x 3 channels = ~1.8 million ints
+"""
+
+prf = cv.imread('PartyRockFire.jpeg', 1)
+
+
+def display(img):
+    """function to show the image"""
+    cv.imshow('Party Rock Fire', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+
+    # set range for "red"
+    redlo = np.array([0, 0, 118], np.uint8)
+    redhi = np.array([100, 100, 255], np.uint8)
+
+    # get red pixels
+    red = cv.inRange(prf, redlo, redhi)
+
+    # loop
+    print((np.where(red > 0)))
+
+    # display(red[red >= 0])
